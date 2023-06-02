c76abb4120a2209c39820c81ae598bcd04db0bf6
kleva-j <kasmickleva@gmail.com>
2023-06-02 10:15:58 +0100

    fix: reset default page layout and stylings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
-import { Metadata } from 'next';
-import './globals.css';
 import { Inter } from 'next/font/google';
+import { Metadata } from 'next';
+
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
