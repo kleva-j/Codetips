5b1a97b3bf5d8b5fd217a21bc008b289df5202f5
kleva-j <kasmickleva@gmail.com>
2023-06-03 16:32:08 +0100

    feat: create layout component displaying all ui component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { Layout } from '@/components/layout';
+
 export default function Home() {
   return (
-    <main className="flex flex-col justify-between items-center p-24"></main>
+    <main className="flex flex-col justify-between items-center p-24">
+      <Layout />
+    </main>
   );
 }
