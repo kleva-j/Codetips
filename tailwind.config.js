84cf5de6c94c70a82aad835bebd858eb3dcd4581
kleva-j <kasmickleva@gmail.com>
2023-06-06 08:34:00 +0100

    feat: extend tailwind theme to include a light-bg color

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,7 @@ module.exports = {
         ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
+        light_bg: 'hsl(var(--dim-background))',
         primary: {
           DEFAULT: 'hsl(var(--primary))',
           foreground: 'hsl(var(--primary-foreground))',
