eb9f1b573bd53bda8532e29b65c07de612a940d2
kleva-j <kasmickleva@gmail.com>
2023-06-06 08:29:30 +0100

    feat: define project data typings

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,13 @@ export enum colorScheme {
   light = 'light',
   dark = 'dark',
 }
+export type ProjectType = {
+  name: string;
+  date: string;
+  slug: string;
+  tagline: string;
+  url?: string;
+  writeup?: string;
+  highlight?: boolean;
+  github_url?: string;
+};
