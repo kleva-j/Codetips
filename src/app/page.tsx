5741826ca392693ca4e71894f46e7592bdb86483
kleva-j <kasmickleva@gmail.com>
2023-06-07 22:14:45 +0100

    feat: implement snippet page and layout component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Layout } from '@/components/layout';
+import { UiLayout } from '@/components/layout';
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-between items-center p-24">
-      <Layout />
+      <UiLayout />
     </main>
   );
 }
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,1 +1,5 @@
-export { Layout } from './ui';
+export { Snippets } from '@/components/layout/snippets';
+export { Sidebar } from '@/components/layout/sidebar';
+export { Layout } from '@/components/layout/project';
+export { Navbar } from '@/components/layout/navbar';
+export { UiLayout } from '@/components/layout/ui';
