6bcb2f1e02463259199df63c0896c9014959fcab
kleva-j <kasmickleva@gmail.com>
2023-06-16 23:00:07 +0100

    feat: modify snippet cards to navigate to single snippet page on click

--- src/components/layout/snippets/snippet-grid.tsx
+++ src/components/layout/snippets/snippet-grid.tsx
@@ -1,3 +1,5 @@
+import NextLink from 'next/link';
+
 import { Heading } from '@/components/ui/heading';
 import { Card } from '@/components/ui/card';
 import { Text } from '@/components/ui/text';
@@ -7,15 +9,21 @@ export const SnippetCardView = () => {
   return (
     <ul className="grid justify-center md:grid-cols-[repeat(2,_1fr)] lg:grid-cols-[repeat(3,_1fr)] gap-5 my-4">
       {snippetList.map((snippet) => (
-        <Card className="p-4" key={snippet.title}>
-          <Heading
-            variant="h4"
-            className="text-rose-500 hover:underline cursor-pointer underline-offset-4"
-          >
-            {snippet.title}
-          </Heading>
-          <Text className="mt-1">{snippet.createdAt}</Text>
-        </Card>
+        <NextLink
+          key={snippet.title}
+          href={`/snippet?id=${snippet.id}`}
+          as={`/snippet/${snippet.id}`}
+        >
+          <Card className="p-4">
+            <Heading
+              variant="h4"
+              className="text-rose-500 hover:underline cursor-pointer underline-offset-4"
+            >
+              {snippet.title}
+            </Heading>
+            <Text className="mt-1">{snippet.createdAt}</Text>
+          </Card>
+        </NextLink>
       ))}
     </ul>
   );
--- src/components/layout/snippets/snippet-list.tsx
+++ src/components/layout/snippets/snippet-list.tsx
@@ -1,5 +1,7 @@
-import { Badge } from '@/components/ui/badge';
+import NextLink from 'next/link';
+
 import { Heading } from '@/components/ui/heading';
+import { Badge } from '@/components/ui/badge';
 import { Text } from '@/components/ui/text';
 import { snippetList } from '@/data';
 
@@ -7,31 +9,34 @@ export const SnippetListView = () => {
   return (
     <ul className="flex flex-col gap-y-4">
       {snippetList.map((snippet) => (
-        <li
-          className="flex flex-col gap-y-2 border border-transparent hover:border-accent p-3 rounded cursor-pointer"
+        <NextLink
           key={snippet.title}
+          href={`/snippet?id=${snippet.id}`}
+          as={`/snippet/${snippet.id}`}
         >
-          <div className="flex justify-between">
-            <Heading
-              variant="h4"
-              className="text-rose-500 hover:underline cursor-pointer"
-            >
-              {snippet.title}
-            </Heading>
-            <Text>{snippet.createdAt}</Text>
-          </div>
-          <div className="flex space-x-3 items-center">
-            {snippet.tags?.map((tag) => (
-              <Badge
-                key={tag}
-                variant="outline"
-                className="cursor-pointer hover:border-rose-500"
+          <li className="flex flex-col gap-y-2 border border-transparent hover:border-accent p-3 rounded cursor-pointer">
+            <div className="flex justify-between">
+              <Heading
+                variant="h4"
+                className="text-rose-500 hover:underline cursor-pointer"
               >
-                {tag}
-              </Badge>
-            ))}
-          </div>
-        </li>
+                {snippet.title}
+              </Heading>
+              <Text>{snippet.createdAt}</Text>
+            </div>
+            <div className="flex space-x-3 items-center">
+              {snippet.tags?.map((tag) => (
+                <Badge
+                  key={tag}
+                  variant="outline"
+                  className="cursor-pointer hover:border-rose-500"
+                >
+                  {tag}
+                </Badge>
+              ))}
+            </div>
+          </li>
+        </NextLink>
       ))}
     </ul>
   );
