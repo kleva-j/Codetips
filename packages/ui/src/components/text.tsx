ab230d0ac4450d217e1a37013092ca74061d050a
kleva-j <kasmickleva@gmail.com>
2023-07-08 22:14:17 +0100

    fix: minor changes to text ui component

--- packages/ui/src/components/text.tsx
+++ packages/ui/src/components/text.tsx
@@ -11,6 +11,7 @@ const textVariants = cva('', {
   variants: {
     variant: {
       p: 'leading-7',
+      span: "leading-7 text-sm",
       blockquote: 'mt-6 border-l-2 pl-6 italic',
       code: 'relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold',
     },
