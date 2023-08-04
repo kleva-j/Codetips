3715bc158992ae8f5fb636b6e1295d1104c857e6
kleva-j <kasmickleva@gmail.com>
2023-08-04 08:13:50 +0100

    fix: minor files and folder restructure

--- apps/web/src/app/(article)/layout.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { ContextProvider } from '@/components/context/provider';
-import { PropsWithChildren } from 'react';
-
-export default function ArticleLayout({ children }: PropsWithChildren) {
-  return <ContextProvider>{children}</ContextProvider>;
-}
